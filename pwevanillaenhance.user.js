c69893adc321a176df05fd9ece9707d054a15d74
Goodlookinguy <Goodlookinguy@nrgs.org>
2015-06-21 18:17:51 -0700

    - Updated color picker

--- pwevanillaenhance.user.js
+++ pwevanillaenhance.user.js
@@ -141,7 +141,7 @@ $(document).ready(function() {
 	$(".editor-action-emoji").after(makeFontColorPicker());
 	$(".editor-action-emoji").after(makePWPanel());
 	
-	$.getScript("https://cdn.rawgit.com/Goodlookinguy/colorpicker/master/js/evol.colorpicker.js", function() {
+	$.getScript("https://cdn.rawgit.com/Goodlookinguy/colorpicker/2acd4b8/js/evol.colorpicker.js", function() {
 		initColorPicker($('.font-color-picker'))
 	});
 	$(document).on( "EditCommentFormLoaded", function(event, container) {
