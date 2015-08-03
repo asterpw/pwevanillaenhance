02d291768d7949bc34bd51232e4cd5b52475578e
Asterelle <asterell@gmail.com>
2015-08-03 10:11:37 -0400

    remove duplicate CO link

--- pwevanillaenhance.user.js
+++ pwevanillaenhance.user.js
@@ -4,7 +4,7 @@
 // @downloadURL https://github.com/asterpw/pwevanillaenhance/raw/master/pwevanillaenhance.user.js
 // @updateURL  https://github.com/asterpw/pwevanillaenhance/raw/master/pwevanillaenhance.user.js
 // @icon http://cd8ba0b44a15c10065fd-24461f391e20b7336331d5789078af53.r23.cf1.rackcdn.com/perfectworld.vanillaforums.com/favicon_2b888861142269ff.ico
-// @version    1.2.2.2
+// @version    1.2.2.3
 // @run-at     document-start
 // @description  Adds useful tools to the pwe vanilla forums
 // @match      http://perfectworld.vanillaforums.com/*
@@ -1438,7 +1438,6 @@ var makeGameLinks = function(container) {
 		"startrekonline|Lore|http://sto.gamepedia.com/Lore",
 		"startrekonline|Commodities|http://sto.gamepedia.com/Commodities",
 		"championsonline|Admin Posts|http://perfectworld.vanillaforums.com/search?adv=1&search=&title=&author=jheinig%2Ccrypticarkayne%2Cladygadfly%2Ctrailturtle%2Csplosions&cat=all&tags=&discussion_d=1&discussion_question=1&discussion_poll=1&comment_c=1&comment_answer=1&within=1+year&date=",
-		"championsonline|Wiki|http://www.championswiki.com/index.php?title=Main_Page",
 		"championsonline|PDB|http://primusdatabase.com",
 		"championsonline|Reddit|http://www.reddit.com/r/ChampionsOnlineFFA/",
 		"championsonline|Wiki|http://www.championswiki.com/index.php?title=Main_Page",
@@ -1737,6 +1736,15 @@ var jQueryLoaded = function() {
 	$(document).on( "EditCommentFormLoaded", function(event, container) {
 		installFeatures(container);
 	});
+	/*$(document).on( "DOMNodeInserted", ".ItemComment", function(event) {
+		var container = $(event.target)
+		if (container.is(".ItemComment")) {
+			redirectUrls();
+			installFeatures(container);
+			applyTitles(container);
+			hideBlockedUsers(container);
+		}
+	});*/
 	$(document).on( "PageLoaded", function(event, container) {
 		redirectUrls();
 		installFeatures(container);
