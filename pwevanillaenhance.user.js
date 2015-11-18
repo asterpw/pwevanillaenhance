d21ac572f3e012bb95732b01a6cd43c4043c838b
Asterelle <asterell@gmail.com>
2015-11-17 23:14:58 -0500

    message about embed issue

--- pwevanillaenhance.user.js
+++ pwevanillaenhance.user.js
@@ -4,7 +4,7 @@
 // @downloadURL https://github.com/asterpw/pwevanillaenhance/raw/master/pwevanillaenhance.user.js
 // @updateURL  https://github.com/asterpw/pwevanillaenhance/raw/master/pwevanillaenhance.user.js
 // @icon http://cd8ba0b44a15c10065fd-24461f391e20b7336331d5789078af53.r23.cf1.rackcdn.com/perfectworld.vanillaforums.com/favicon_2b888861142269ff.ico
-// @version    1.4.0.1
+// @version    1.4.0.2
 // @run-at     document-start
 // @description  Adds useful tools to the pwe vanilla forums
 // @match      http://forum.arcgames.com/*
@@ -13,7 +13,7 @@
 // ==/UserScript==
 
 (function() {	
-var VERSION = "1.4.0";  //what we store when we should display what's new dialog
+var VERSION = "1.4.0.2";  //what we store when we should display what's new dialog
 var getFullVersion = function() { // For version display on the screen;
 	try {
 		return GM_info.script.version;  //causes error if not supported
@@ -23,6 +23,7 @@ var getFullVersion = function() { // For version display on the screen;
 };
 /*jshint multistr: true */
 var CHANGELOG = " \
+	Hi Enhance users!  I'm aware of the new redirect issue when browsing natively and am working on a fix.  For now you may want to use the embedded version of the forums.<br><br>\
 	<div class='change-ver'>v1.4.0</div> - Fixed menus on PWE's new forum theme<br> - Added new 'hat' emotes to FW Emotes <br>\
 	- Enhance Theme Makers Need to Update their Themes! \
 	<!-- <div class='change-ver'>v1.4.0</div> - Added Discussion Filter / Navigation from <a href='http://forum.arcgames.com/arc/profile/eiledon'>@eiledon</a>! -->\
